f51e76ac97b2616ed85902e018400feb18572ed1
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-02 17:44:17 +0100

    Added my name to a C# file in contributors

---
+++ contributors/EvieSkinner.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvieSkinnerHacktoberfest
+{
+    class EvieSkinner
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Evie-Skinner18");
+            Console.WriteLine("Happy Hacktoberfest! My name's Evie and I am interested in working on Ruby, C# .Net or general front end projects.");
+            Console.ReadKey();
+        }
+    }
+}

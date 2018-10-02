483892fbd057edd9797558a5cead9e0deb2df0e2
Irfan Radzi <rolodex@live.co.uk>
2018-10-02 09:20:20 +0800

    Entry!
    I'm entering

---
+++ contributors/r0lodex.js
@@ -0,0 +1,7 @@
+function gimmegimme() {
+  const name = "r0lodex";
+  const lang = "javascript"
+  return `Hello everyone, ${name} here would hack October with ${lang}`
+}
+
+console.log(gimmegimme())

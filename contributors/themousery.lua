fe51778f2f456e5b2f736b253779c33c5690cab0
themousery <mouserygames@gmail.com>
2018-10-01 17:53:17 -0700

    themousery's contributer file :)

---
+++ contributors/themousery.lua
@@ -0,0 +1,5 @@
+function thing()
+  print("Hello! I'm themousery and I am expierenced in Lua (specifically with LÖVE2D) but I also have expierence with Python, Javascript (and HTML/CSS of course), and minimal expierence with Java and C#.")
+end
+
+thing()

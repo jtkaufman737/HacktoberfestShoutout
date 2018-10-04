09fe5b01f3765cfc5e0da15b0e8f3f820c5381a0
Ritik <ritikgupta4@gmail.com>
2018-10-04 06:50:20 +0530

    File created!

---
+++ contributors/ritikgupta4.py
@@ -0,0 +1,1 @@
+print "Hi! My name is Ritik Gupta and I want to contribute to projects in Python and Java. I hope to get good with contributing to the open source community as soon as possible. Thanks!"

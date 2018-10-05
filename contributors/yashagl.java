d57ab62ccfcc9e1e68fcd5e0af6f2b8b41d84c59
Yash Agarwal <yashagl1620@gmail.com>
2018-10-05 22:49:37 +0530

    Added yashagl.java

---
+++ contributors/yashagl.java
@@ -0,0 +1,7 @@
+class yashagl
+{
+  public static void main(String [] args)
+  {
+    System.out.println("Hello World, I am Yash from India. \nYou can find me at https://github.com/yashagldit. \nI am most interested in Android Development.");
+  }
+}

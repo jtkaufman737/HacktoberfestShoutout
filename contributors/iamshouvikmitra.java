508a1819c3016429e8707fd0aeac7ceb9d6808c0
Shouvik Mitra <shouvikmitra@outlook.com>
2018-10-05 20:08:55 +0530

    Update and rename shouvik.java to iamshouvikmitra.java

---
+++ contributors/iamshouvikmitra.java
@@ -0,0 +1,7 @@
+public class Shouvik
+{
+  public static void main(String [] args)
+  {
+    System.out.println("Hello World, I am Shouvik Mitra from India. \nYou can find me at https://github.com/iamshouvikmitra. \nI am most interested in Web Development.");
+  }
+}
--- contributors/shouvik.java
+++
@@ -1,7 +0,0 @@
-public class Shouvik
-{
-  public static void main(String [] args)
-  {
-    System.out.println("Hello World, I am Shouvik Mitra from India");
-  }
-}

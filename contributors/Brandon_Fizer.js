8f004cda5b725accd117888028113c36914c1128
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-10-12 19:43:05 -0400

    Track7Dev | Intro

---
+++ contributors/Brandon_Fizer.js
@@ -0,0 +1,24 @@
+console.log(
+"\n"+
+"\n"+
+"    __  __              __    __          __                 ____             __\n"+
+"   / / / /____ _ _____ / /__ / /_ ____   / /_   ___   _____ / __/___   _____ / /_\n"+
+"  / /_/ // __ `// ___// //_// __// __ \\ / __ \\ / _ \\ / ___// /_ / _ \\ / ___// __/\n"+
+" / __  // /_/ // /__ / ,<  / /_ / /_/ // /_/ //  __// /   / __//  __/(__  )/ /_\n"+
+"/_/ /_/ \\__,_/ \\___//_/|_| \\__/ \\____//_.___/ \\___//_/   /_/   \\___//____/ \\__/\n" +
+"=================================================================================\n"+
+"2018\n"+
+"\n"+
+"- BRANDON FIZER @Track7Dev\n"+
+"- Full-Stack Software Engineer \n"+
+"- Github: https://github.com/Track7Dev\n"+
+"- LinkedIn: https://www.linkedin.com/in/brandon-fizer/\n"+
+"\n"+
+" I'm interested in working with a large variety of different projects, nothing really\n"+
+"specific. I work mainly with javascript on the MERN stack, but have had experience \n" + 
+"working with python, C, C#, ruby. \n"+
+"\n"+
+"Have A GREAT Hacktoberfest! And Have Fun Hacking!!\n"+
+"\n"+
+"\n"
+);

c07c175897573dba83c6a52b6996a94f8aa4106a
Jeff Horner <jhorner3208@gmail.com>
2018-10-14 08:28:04 -0500

    Create jhorner32.js

---
+++ contributors/jhorner32.js
@@ -0,0 +1,4 @@
+const myName = `Jeff Horner`;
+const message = `My name is ${myName} and I would love to work on projects that are javascript related with a focus on vanilla or react.`;
+
+console.log(message);

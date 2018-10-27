2653a21982cce2037c4c82cab3e9c10c712d2dbe
EgehanGundogdu <egehan.gundogdu@icloud.com>
2018-10-27 15:24:59 +0300

    hello its me!

---
+++ contributors/EgehanGundogdu.py
@@ -0,0 +1,10 @@
+
+
+
+def helloworld():
+
+    print("Hi i'm Egehan Gundogdu. My github name is EgehanGundogdu.")
+    print("I love python and ruby. I insterested open source web projects! ")
+
+
+helloworld()

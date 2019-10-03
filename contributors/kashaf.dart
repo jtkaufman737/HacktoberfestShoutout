f2a71958d1f84325aecb78fe1b7ec18f82442b93
Kashaf <akashaf@hermo.my>
2019-10-03 14:50:05 +0800

    add my name in the list for hacktoberfest shoutout 2019

---
+++ contributors/kashaf.dart
@@ -0,0 +1,10 @@
+void main() {
+    print(hacktoberFest('Akashaf'));
+}
+
+hacktoberFest(name) { 
+    String name = "Akashaf Khomarudin";
+    String lang = "dart";
+    String github = "https://github.com/akashaf";
+    return 'Hello everyone, '+name+' here would hack October 2019 with '+lang+' im pretty new with this language. Have fun code and follow me on github '+ github;
+} 

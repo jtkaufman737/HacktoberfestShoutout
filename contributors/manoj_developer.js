7dfcfe2a0376a7dec028a0ec19cb0be477722b03
Manoj <manoj.antony32@gmail.com>
2019-10-21 06:45:15 +0530

    Added Github id

--- contributors/manoj_developer.js
+++ contributors/manoj_developer.js
@@ -1,1 +1,3 @@
-console.log('Hi i am Full Stack Developer, deep learning on React Js. Like to contribute React Js projects.')
+console.log('Github Id: manojantony32');
+console.log('Hi i am Full Stack Developer, deep learning on React Js. Like to contribute React Js projects.');
+console.log('Profile link: https://github.com/manojantony32');

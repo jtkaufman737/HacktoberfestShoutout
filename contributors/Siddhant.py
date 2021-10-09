e4dc11c68598dc78fb49dc9e7e1fa98ae924310c
siddhantS1504 <siddhantsaxena1504@gmail.com>
2021-10-09 12:54:38 +0530

    Added Siddhant.py

---
+++ contributors/Siddhant.py
@@ -0,0 +1,8 @@
+def intro():
+  
+  print("Hello, My username is SiddhantS1504")
+  print("I am interested to contribute in machine learning and web development projects")
+  
+  
+  
+def intro()  
